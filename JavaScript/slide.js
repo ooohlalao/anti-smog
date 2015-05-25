94715ffbd82445c9b3cb368bb7db15147d6042fa
ooohlalao <Namesallbeentaken@gmail.com>
2015-05-25 20:53:57 +0800

    add four detail pages

--- JavaScript/slide.js
+++ JavaScript/slide.js
@@ -35,11 +35,11 @@ function changePicture(direction)
 function initLists()
 {	
 	imglist=['img1.jpg','img2.jpg','img3.jpg','img4.jpg'];
-	titlelist=['BEIJING AIR POLLUTION','10 FACTS ABOUT SMOG','BEST SOLUTIONS','SPREAD OUR SPIRITS WITH STYLES'];
-	hreflist=['pages/1.html','pages/2.html','pages/3.html','pages/4.html'];
-	var text1="The Chinese capital has for many years suffered from serious air pollution. Primary sources of pollutants include exhaust emission from Beijing's more than five million motor vehicles, coal burning in neighbouring regions, dust storms from the north and local construction dust...... &nbsp;&nbsp;";
-	var text2="The Chinese capital has for many years suffered from serious air pollution. Primary sources of pollutants include exhaust emission from Beijing's more than five million motor vehicles, coal burning in neighbouring regions, dust storms from the north and local construction dust...... &nbsp;&nbsp;";
-	var text3="The Chinese capital has for many years suffered from serious air pollution. Primary sources of pollutants include exhaust emission from Beijing's more than five million motor vehicles, coal burning in neighbouring regions, dust storms from the north and local construction dust...... &nbsp;&nbsp;";
+	titlelist=['BEIJING AIR POLLUTION','40 FACTS ABOUT SMOG','BEST SOLUTIONS','SPREAD OUR SPIRITS WITH STYLES'];
+	hreflist=['1.html','2.html','3.html','4.html'];
+	var text1="The Chinese capital has for many years suffered from serious air pollution. Primary sources of pollutants include exhaust emission from Beijing's more than five million motor vehicles, coal burning in neighbouring regions, dust storms from the north and local construction dust... &nbsp;&nbsp;";
+	var text2="	Everyone on earth knows that air pollution is hazardous to health. The effects of air pollution can have devastating effects on your health and 		the environment... ";
+	var text3="We make choices everyday that can help reduce air pollution. Below are a few ideas that you can take to help clean our air... &nbsp;&nbsp;";
 	var text4="The Chinese capital has for many years suffered from serious air pollution. Primary sources of pollutants include exhaust emission from Beijing's more than five million motor vehicles, coal burning in neighbouring regions, dust storms from the north and local construction dust...... &nbsp;&nbsp;";
 	
 	textlist.push(text1);
