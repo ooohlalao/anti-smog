64bee314806fde8739bff3596e19e17eeb6aed2c
ooohlalao <Nameallbeentaken@gmail.com>
2015-05-26 21:54:01 +0800

    edit news pages
    add 5.html,
    modify 1~4.html.
    add news page as a news container.

--- JavaScript/slide.js
+++ JavaScript/slide.js
@@ -19,12 +19,12 @@ function changePicture(direction)
     curimg+=direction;
 	if(curimg>imglist.length-1) curimg=0;
 	if(curimg<0) curimg=imglist.length-1;
-	
+
 	bground.style.backgroundImage='url(images/'+imglist[curimg]+')';
 	// foreground.src='images/'+imglist[curimg];
     imgs[curimg].style.display="block";
     foreground.href=hreflist[curimg];
-	
+
 	picMinibar.innerHTML=titlelist[curimg];
     picMinibar.href=hreflist[curimg];
 	var hreftext='<a class="readmore" href="'+hreflist[curimg]+'">[Read More]</a>';
@@ -35,12 +35,12 @@ function changePicture(direction)
 function initLists()
 {	
 	imglist=['img1.jpg','img2.jpg','img3.jpg','img4.jpg'];
-	titlelist=['BEIJING AIR POLLUTION','40 FACTS ABOUT SMOG','BEST SOLUTIONS','SPREAD OUR SPIRITS WITH STYLES'];
+	titlelist=['CARS OR COAL?','40 FACTS ABOUT SMOG','BEST SOLUTIONS','SPREAD OUR SPIRITS WITH STYLES'];
 	hreflist=['1.html','2.html','3.html','4.html'];
 	var text1="The Chinese capital has for many years suffered from serious air pollution. Primary sources of pollutants include exhaust emission from Beijing's more than five million motor vehicles, coal burning in neighbouring regions, dust storms from the north and local construction dust... &nbsp;&nbsp;";
 	var text2="	Everyone on earth knows that air pollution is hazardous to health. The effects of air pollution can have devastating effects on your health and 		the environment... ";
 	var text3="We make choices everyday that can help reduce air pollution. Below are a few ideas that you can take to help clean our air... &nbsp;&nbsp;";
-	var text4="The Chinese capital has for many years suffered from serious air pollution. Primary sources of pollutants include exhaust emission from Beijing's more than five million motor vehicles, coal burning in neighbouring regions, dust storms from the north and local construction dust...... &nbsp;&nbsp;";
+	var text4="The Chinese capital has for many years suffered from serious air pollution. Primary sources of pollutants include exhaust emission from Beijing's more than five million motor vehicles, coal burning in neighbouring regions, dust storms from the north and local construction dust... &nbsp;&nbsp;";
 	
 	textlist.push(text1);
 	textlist.push(text2);
